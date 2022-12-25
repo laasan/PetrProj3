252e44e41598dc8768ab218c3af0ebaba4ad518d
laasan <alekalx@gmail.com>
2022-12-25 20:26:44 +0300

    bug fix with video on index.html

--- dist/script.js
+++ dist/script.js
@@ -5655,14 +5655,16 @@ function () {
       var _this = this;
 
       this.btns.forEach(function (btn, i) {
-        var blockedElem = btn.closest('.module__video-item').nextElementSibling;
+        try {
+          var blockedElem = btn.closest('.module__video-item').nextElementSibling;
 
-        if (i % 2 == 0) {
-          blockedElem.setAttribute('data-disabled', 'true');
-        }
+          if (i % 2 == 0) {
+            blockedElem.setAttribute('data-disabled', 'true');
+          }
+        } catch (error) {}
 
         btn.addEventListener('click', function () {
-          if (btn.closest('.module__video-item').getAttribute('data-disabled') !== 'true') {
+          if (!btn.closest('.module__video-item') || btn.closest('.module__video-item').getAttribute('data-disabled') !== 'true') {
             _this.activeBtn = btn;
 
             if (document.querySelector('iframe#frame')) {
@@ -5711,21 +5713,23 @@ function () {
   }, {
     key: "onPlayerStateChange",
     value: function onPlayerStateChange(state) {
-      var blockedElem = this.activeBtn.closest('.module__video-item').nextElementSibling;
-      var playBtn = this.activeBtn.querySelector('svg').cloneNode(true);
+      try {
+        var blockedElem = this.activeBtn.closest('.module__video-item').nextElementSibling;
+        var playBtn = this.activeBtn.querySelector('svg').cloneNode(true);
 
-      if (state.data === 0) {
-        if (blockedElem.querySelector('.play__circle').classList.contains('closed')) {
-          blockedElem.querySelector('.play__circle').classList.remove('closed');
-          blockedElem.querySelector('svg').remove();
-          blockedElem.querySelector('.play__circle').appendChild(playBtn);
-          blockedElem.querySelector('.play__text').textContent = 'play video';
-          blockedElem.querySelector('.play__text').classList.remove('attention');
-          blockedElem.style.opacity = 1;
-          blockedElem.style.filter = 'none';
-          blockedElem.setAttribute('data-disabled', 'false');
+        if (state.data === 0) {
+          if (blockedElem.querySelector('.play__circle').classList.contains('closed')) {
+            blockedElem.querySelector('.play__circle').classList.remove('closed');
+            blockedElem.querySelector('svg').remove();
+            blockedElem.querySelector('.play__circle').appendChild(playBtn);
+            blockedElem.querySelector('.play__text').textContent = 'play video';
+            blockedElem.querySelector('.play__text').classList.remove('attention');
+            blockedElem.style.opacity = 1;
+            blockedElem.style.filter = 'none';
+            blockedElem.setAttribute('data-disabled', 'false');
+          }
         }
-      }
+      } catch (error) {}
     }
   }, {
     key: "init",
